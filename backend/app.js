8df1fabfda7c00c2c71a9738e97a72ac70bf7567
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-12-24 20:15:19 +0530

    install morgan to check from which route the requesting is coming

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
-import express, { urlencoded } from "express";
+import express from "express";
+import morgan from "morgan";
 
 const app = express();
-
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
