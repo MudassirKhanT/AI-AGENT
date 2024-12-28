d354e28472394dec407802a6348f51e5b5bc0644
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-12-28 19:43:09 +0530

    Login Route

--- backend/route/user.routes.js
+++ backend/route/user.routes.js
@@ -2,13 +2,8 @@ import { Router } from "express";
 import * as userController from "../controllers/user.controller.js";
 import { body } from "express-validator";
 const router = Router();
-
-router.post(
-  "/register",
-  body("email").isEmail().withMessage("Email must be a valid email addresss"),
-  body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
-
-  userController.createUserController
-);
-
+//register
+router.post("/register", body("email").isEmail().withMessage("Email must be a valid email addresss"), body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"), userController.createUserController);
+//Login
+router.post("/login", body("email").isEmail().withMessage("Email must be a valid email addresss"), body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"), userController.loginController);
 export default router;
