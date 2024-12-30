a54617b06acda36cc0f3a1c107bd68dc379c7bc8
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-12-30 17:10:24 +0530

    added message ui

--- Ai_Frontend/src/screens/Project.jsx
+++ Ai_Frontend/src/screens/Project.jsx
@@ -6,12 +6,30 @@ const Project = () => {
 
   return (
     <main className="h-screen w-screen flex">
-      <section className="left h-full min-w-60 bg-red-300">
-        <header className="flex justify-end p-2 px-4 w-full bg-slate-200">
+      <section className="left flex flex-col h-full min-w-72 bg-slate-300">
+        <header className="flex justify-end p-2 px-4 w-full bg-slate-100">
           <button className="p-2 ">
             <i className="ri-group-fill"></i>
           </button>
         </header>
+        <div className="conversation-area flex-grow flex flex-col">
+          <div className="message-box p-1 flex-grow flex flex-col gap-1">
+            <div className=" message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+              <small className="opacity-65 text-xs">example@gmail.com</small>
+              <p className="text-sm">Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="ml-auto message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+              <small className="opacity-65 text-xs">example@gmail.com</small>
+              <p className="text-sm">Lorem ipsum dolor sit amet.</p>
+            </div>
+          </div>
+          <div className="input-field w-full flex">
+            <input className="p-2 px-4 border-none outline-none" type="text" placeholder="Enter message" />
+            <button className="flex-grow px-2 ">
+              <i className="ri-send-plane-fill"></i>
+            </button>
+          </div>
+        </div>
       </section>
     </main>
   );
